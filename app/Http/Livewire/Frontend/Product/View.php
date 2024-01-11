e6d640ba9ec27d7b22cd2d2982b1872e0a2bdd09
sagarmintu <sagarskbehera1996@gmail.com>
2024-01-11 17:37:46 +0530

    adding products in wishlist

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -3,11 +3,39 @@
 namespace App\Http\Livewire\Frontend\Product;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Wishlist;
 
 class View extends Component
 {
     public $category, $product, $prdColorSelectedQuantity;
 
+    public function addToWishlist($productId)
+    {
+        // dd($productId);
+        if (Auth::check())
+        {
+            // dd("I am in");
+            if(Wishlist::where('user_id', auth()->user()->id)->where('product_id', $productId)->exists())
+            {
+                session()->flash('message', 'Already Added To Wishlist');
+                return false;
+            }
+            else
+            {
+                Wishlist::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id' => $productId
+                ]);
+                session()->flash('message', 'Wishlist Added Successfully');
+            }
+        }
+        else
+        {
+            session()->flash('message', 'Please Login To Continue...');
+            return false;
+        }
+    }
     public function colorSelected($productColorId)
     {
         // dd($productColorId);
--- resources/views/livewire/frontend/product/view.blade.php
+++ resources/views/livewire/frontend/product/view.blade.php
@@ -1,11 +1,19 @@
 <div>
     <div class="py-3 py-md-5">
         <div class="container">
+
+            @if (session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
+                </div>
+            @endif
+
             <div class="row">
                 <div class="col-md-5 mt-3">
                     <div class="bg-white border">
                         @if ($product->productImages)
-                            <img src="{{ asset($product->productImages[0]->image) }}" class="w-100" alt="Img" style="width: 180px; height: 450px;">
+                        <img src="{{ asset($product->productImages[0]->image) }}" class="w-100" alt="Img"
+                            style="width: 180px; height: 450px;">
                         @else
                         <h5 class="text-danger">No Image Added !!!</h5>
                         @endif
@@ -18,11 +26,11 @@
 
                             @if($product->productColors->count() > 0)
 
-                                @if($product->quantity)
-                                    <label class="btn-sm py-1 mt-2 text-white bg-success">In Stock</label>
-                                @else
-                                    <label class="btn-sm py-1 mt-2 text-white bg-danger">Out Of Stock</label>
-                                @endif
+                            @if($product->quantity)
+                            <label class="btn-sm py-1 mt-2 text-white bg-success">In Stock</label>
+                            @else
+                            <label class="btn-sm py-1 mt-2 text-white bg-danger">Out Of Stock</label>
+                            @endif
 
                             @endif
 
@@ -40,24 +48,23 @@
 
                             <!-- @if($product->productColors->count() > 0) -->
 
-                                @if($product->productColors)
-                                    @foreach ($product->productColors as $colorItem)
-                                        <!-- <input type="radio" name="colorSelection" value="{{ $colorItem->id }}" /> {{ $colorItem->color->name }} -->
-                                        <label class="colorSelectionLabel" style="{{ $colorItem->color->code }}"
-                                        wire:click="colorSelected({{ $colorItem->id }})"
-                                        >
-                                            {{ $colorItem->color->name }}
-                                        </label>
-                                    @endforeach
-                                @endif
+                            @if($product->productColors)
+                                @foreach ($product->productColors as $colorItem)
+                                <!-- <input type="radio" name="colorSelection" value="{{ $colorItem->id }}" /> {{ $colorItem->color->name }} -->
+                                <label class="colorSelectionLabel" style="{{ $colorItem->color->code }}"
+                                    wire:click="colorSelected({{ $colorItem->id }})">
+                                    {{ $colorItem->color->name }}
+                                </label>
+                                @endforeach
+                            @endif
 
-                                <div>
-                                    @if ($this->prdColorSelectedQuantity == 'outOfStock')
-                                        <label class="btn-sm py-1 mt-2 text-white bg-danger">Out Of Stock</label>
-                                    @elseif ($this->prdColorSelectedQuantity > 0)
-                                        <label class="btn-sm py-1 mt-2 text-white bg-success">In Stock</label>
-                                    @endif
-                                </div>
+                            <div>
+                                @if ($this->prdColorSelectedQuantity == 'outOfStock')
+                                    <label class="btn-sm py-1 mt-2 text-white bg-danger">Out Of Stock</label>
+                                @elseif ($this->prdColorSelectedQuantity > 0)
+                                    <label class="btn-sm py-1 mt-2 text-white bg-success">In Stock</label>
+                                @endif
+                            </div>
 
                             <!-- @else
 
@@ -81,7 +88,12 @@
                         </div>
                         <div class="mt-2">
                             <a href="" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
-                            <a href="" class="btn btn1"> <i class="fa fa-heart"></i> Add To Wishlist </a>
+                            <button type="button" wire:click="addToWishlist({{ $product->id }})" class="btn btn1"> 
+                                <span wire:loading.remove>
+                                    <i class="fa fa-heart"></i> Add To Wishlist 
+                                </span>
+                                <span wire:loading wire:target="addToWishlist">Adding ...</span>
+                            </button>
                         </div>
                         <div class="mt-3">
                             <h5 class="mb-0">Small Description</h5>
