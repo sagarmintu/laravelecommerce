c1130776d3bab70cf4c250d0badb08e220bf8b07
sagarmintu <sagarskbehera1996@gmail.com>
2024-01-12 10:50:21 +0530

    remove wishlist using livewire

--- app/Http/Livewire/Frontend/WishlistShow.php
+++ app/Http/Livewire/Frontend/WishlistShow.php
@@ -7,6 +7,16 @@ use App\Models\Wishlist;
 
 class WishlistShow extends Component
 {
+    public function removeWishlistItem(int $wishlistId)
+    {
+        // dd($wishlistId);
+        Wishlist::where('user_id', auth()->user()->id)->where('id', $wishlistId)->delete();
+        $this->dispatchBrowserEvent('message', [
+            'text' => 'Wishlist Item Deleted Successfully',
+            'type' => 'success',
+            'status' => 200
+        ]);
+    }
     public function render()
     {
         $wishlist = Wishlist::where('user_id', auth()->user()->id)->get();
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,10 @@ Route::get('/collections', [App\Http\Controllers\Frontend\FrontendController::cl
 Route::get('/collections/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'products']);
 Route::get('/collections/{category_slug}/{product_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'productView']);
 
-Route::get('wishlist', [App\Http\Controllers\Frontend\WishlistController::class, 'index']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('wishlist', [App\Http\Controllers\Frontend\WishlistController::class, 'index']);
+});
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
