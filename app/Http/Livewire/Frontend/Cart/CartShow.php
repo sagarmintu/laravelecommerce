496fa968c93585552ae097f1a559ef2a88ec7c68
sagarmintu <sagarskbehera1996@gmail.com>
2024-01-18 11:13:38 +0530

    Shopping cart item Quantity increment decrement using Livewire

--- app/Http/Livewire/Frontend/Cart/CartShow.php
+++ app/Http/Livewire/Frontend/Cart/CartShow.php
@@ -8,6 +8,121 @@ use App\Models\Cart;
 class CartShow extends Component
 {
     public $cart;
+
+    public function decrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id',$cartId)->where('user_id', auth()->user()->id)->first();
+        if ($cartData)
+        {
+            if ($cartData->productColor()->where('id',$cartData->product_color_id)->exists())
+            {
+                $productColor = $cartData->productColor()->where('id',$cartData->product_color_id)->first();
+                if ($productColor->quantity > $cartData->quantity)
+                {
+                    $cartData->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+                else
+                {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' .$productColor->quantity. ' Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }
+            else
+            {
+                if ($cartData->product->quantity > $cartData->quantity)
+                {
+                    $cartData->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+                else
+                {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' .$cartData->product->quantity. ' Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }
+        }
+        else
+        {
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Something Went Wrong !!!',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
+    }
+
+    public function incrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id',$cartId)->where('user_id', auth()->user()->id)->first();
+        if ($cartData)
+        {
+            if ($cartData->productColor()->where('id',$cartData->product_color_id)->exists())
+            {
+                $productColor = $cartData->productColor()->where('id',$cartData->product_color_id)->first();
+                if ($productColor->quantity > $cartData->quantity)
+                {
+                    $cartData->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+                else
+                {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' .$productColor->quantity. ' Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }
+            else
+            {
+                if ($cartData->product->quantity > $cartData->quantity)
+                {
+                    $cartData->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'success',
+                        'status' => 200
+                    ]);
+                }
+                else
+                {
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only ' .$cartData->product->quantity. ' Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }
+        }
+        else
+        {
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Something Went Wrong !!!',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
+    }
+
     public function render()
     {
         $this->cart = Cart::where('user_id', auth()->user()->id)->get();
