f7dded88abdeaa903ac8bded56ea0b3349744a09
sagarmintu <sagarskbehera1996@gmail.com>
2024-02-07 12:18:51 +0530

    password updation

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('orders/{orderId}', [App\Http\Controllers\Frontend\OrderController::class, 'show']);
     Route::get('profile', [App\Http\Controllers\Frontend\UserController::class, 'index']);
     Route::post('profile', [App\Http\Controllers\Frontend\UserController::class, 'updateUserDetails']);
+    Route::get('change-password', [App\Http\Controllers\Frontend\UserController::class, 'passwordCreate']);
+    Route::post('change-password', [App\Http\Controllers\Frontend\UserController::class, 'changePassword']);
 });
 
 Route::get('thank-you', [App\Http\Controllers\Frontend\FrontendController::class, 'thankyou']);
